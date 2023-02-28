5fb55ad555cfed8b11a24c58e6753a56c14427c2
Brajesh <bjbrajeshsingh@gmail.com>
2023-02-28 11:50:19 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Welcome !!! , This is an sample app to demonstrate code pipeline in AWS. This is first deployment');
+});
+
+const port = process.env.PORT || 8080;
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
